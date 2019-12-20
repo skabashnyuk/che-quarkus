23113f9899411b6347e991972fa4bc4ff17c42dd
Sergii Kabashniuk <skabashniuk@redhat.com>
2019-12-20 13:03:30 +0100

    Refactor tests platform

---
+++ che-di/deployment/src/main/java/io/quarkus/che/di/deployment/CheDiProcessor.java
@@ -0,0 +1,47 @@
+package io.quarkus.che.di.deployment;
+
+import io.quarkus.arc.processor.AnnotationsTransformer;
+import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.builditem.FeatureBuildItem;
+import io.quarkus.arc.deployment.AdditionalStereotypeBuildItem;
+import io.quarkus.arc.deployment.AnnotationsTransformerBuildItem;
+import io.quarkus.arc.deployment.BeanArchiveIndexBuildItem;
+import io.quarkus.deployment.annotations.BuildProducer;
+import org.jboss.jandex.AnnotationInstance;
+import org.jboss.jandex.DotName;
+import org.jboss.jandex.IndexView;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+
+class CheDiProcessor {
+
+    private static final String FEATURE = "che-di";
+
+    @BuildStep
+    FeatureBuildItem feature() {
+        return new FeatureBuildItem(FEATURE);
+    }
+
+    @BuildStep
+    AnnotationsTransformerBuildItem beanTransformer(
+            final BeanArchiveIndexBuildItem beanArchiveIndexBuildItem,
+            final BuildProducer<AdditionalStereotypeBuildItem> additionalStereotypeBuildItemBuildProducer) {
+        return new AnnotationsTransformerBuildItem(new AnnotationsTransformer() {
+
+            public boolean appliesTo(org.jboss.jandex.AnnotationTarget.Kind kind) {
+                return kind == org.jboss.jandex.AnnotationTarget.Kind.CLASS;
+            }
+
+            public void transform(TransformationContext context) {
+                if (context.getTarget().asClass().name().toString().equals("com.foo.Bar")) {
+                    context.transform().add(MyInterceptorBinding.class).done();
+                }
+            }
+        });
+    }
+}
--- che-quarkus-testservice/src/main/java/org/acme/config/GreetingResource.java
+++ che-quarkus-testservice/src/main/java/org/acme/config/GreetingResource.java
@@ -2,6 +2,7 @@ package org.acme.config;
 
 import java.util.Optional;
 
+import javax.inject.Named;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -12,7 +13,8 @@ import org.eclipse.microprofile.config.inject.ConfigProperty;
 @Path("/greeting")
 public class GreetingResource {
 
-    @ConfigProperty(name = "greeting.message")
+    //@ConfigProperty(name = "greeting.message")
+    @Named("greeting.message")
     String message;
 
 //    @ConfigProperty(name = "greeting.suffix", defaultValue="!")
